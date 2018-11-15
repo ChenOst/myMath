4df6b0ab36d6cde1dadd2bd61e7621cbdf5e1833
EtiReznikov <44700034+EtiReznikov@users.noreply.github.com>
2018-11-15 19:14:22 +0200

    Add files via upload

---
+++ src/myMath/Monom.java
@@ -0,0 +1,309 @@
+package myMath;
+import java.util.Scanner;
+/**
+ * This class represents a simple "Monom" of shape a*x^b, where a is a real number and b is an integer (summed a none negative), 
+ * see: https://en.wikipedia.org/wiki/Monomial 
+ * The class implements function and support simple operations as: construction, value at x, derivative, add and multiply. 
+ * 
+ * @author Chen Ostrovski 316402650 , Ester Reznikov 315674028
+ */
+public class Monom implements function{
+
+	//constructors
+
+	/**
+	 * Regular Constructor : builds a new Monom   
+	 * @param  a  the value of the coefficient
+	 * @param  b  the value of the power
+	 * @throws Exception if the powers value is lower than zero
+	 */
+
+	public Monom(double a, int b) throws Exception  {
+		this._coefficient=a;
+		this.set_power(b);
+
+	}
+
+	/**
+	 * Default Constructor : builds a new Monom.
+	 * Sets the value of the coefficient to be 0,
+	 * Sets the value of the power to be 0
+	 */
+
+	public Monom() {
+		this._coefficient=0;
+		this._power=0;
+	}
+
+	/**
+	 * Copy Constructor : deep copy, builds a new Monom
+	 * @param  other get the coefficient and power of other Monom and set them in this Monom
+	 * @throws Exception if the powers value is lower than zero
+	 */
+
+	public Monom(Monom other) throws Exception {
+		this(other.get_coefficient(), other.get_power());
+	}
+
+	/**
+	 * String Constructor : builds a new Monom.
+	 * Get String as input and change it to Monom
+	 * @param monom proper input a*x^b 
+	 * @throws Exception if the Monom equal to null
+	 * @throws Exeption if the Monom equal to invalid String
+	 */
+
+	public Monom(String monom) throws Exception{
+		// Monom equals to null throw exception
+		if (monom==null)
+			throw new Exception("String is null");
+		// Monom equals to invalid String throw exception
+		else if (monom.matches(".*[!-'].*" )|| monom.matches(".*[,].*" ) || monom.matches(".*[/].*") ||monom.matches(".*[:-W].*")|| monom.matches(".*[Y-]].*") ||
+				monom.matches(".*[_-w].*") || monom.matches(".*[y-~].*") )
+			throw new Exception("String is not legal");
+		// Monom is equal to a legal String 
+		else {
+			monom=monom.replaceAll("[(,), ,*]","");
+			monom=monom.replaceAll("[-]"+"[x]", "-1x");
+			// Checking if the Monom of the form : 'x', which means that coefficient equals to 1
+			if ('x'==monom.charAt(0) || 'X'==monom.charAt(0)){
+				this.set_coefficient(1);
+				// Checking if both coefficient and power equal to 1
+				if(monom.length()==1) {
+					this.set_power(1);
+				}
+				// If power don't equals to 1
+				else
+					this.set_power(Integer.parseInt(monom.substring(2,monom.length())));
+			}
+			// Checking if the Monom contains 'x' or 'X'
+			else if (!monom.contains("x") && !monom.contains("X")) {
+				this.set_coefficient(Double.parseDouble(monom));
+				this.set_power(0);
+			}
+			// Checking if the Monom contains '^'
+			else if (!monom.contains("^")){
+				this.set_coefficient(Double.parseDouble(monom.substring(0,monom.length()-1)));
+				this.set_power(1);
+			}
+			// Checking if the Monom of the form : 'x^b'
+			else {
+				for (int i=0; i<monom.length(); i++) {
+					if (monom.charAt(i)=='x' || monom.charAt(i)=='X') {
+						this.set_coefficient(Double.parseDouble(monom.substring(0,i)));
+					}
+					else if (monom.charAt(i)=='^')	
+						this.set_power(Integer.parseInt(monom.substring(i+1)));
+				}
+			}
+		}
+	}
+
+	//Methods
+
+	/**
+	 * This method get a real number and calculates the Monom value
+	 * @param  x the value of the number
+	 * @return the value of the Monom at x
+	 */
+
+	@Override
+	public double f(double x) {
+		return _coefficient*Math.pow(x, _power);
+	}
+
+	/**
+	 * This method add other Monom to this Monom.
+	 * Add only if the Monoms have the same power
+	 * @param other the other Monom's coefficient and power
+	 * @throws Exception if the Monoms have different power
+	 */
+
+	public void add(Monom other) throws Exception{
+		// Add only the coefficients, the power stays the same
+		// Adds on condition that both Monoms have the same power or one of them is equal to zero
+		if (this.get_coefficient()==0 || other.get_coefficient()==0) {
+			if (this.get_coefficient()==0) {
+				this.set_coefficient(other.get_coefficient());
+				this.set_power(other.get_power());
+			}
+		}
+
+		else if (this.get_power()!=other.get_power())
+			throw new Exception("The power of the monoms is diffrent");
+
+		else 
+			this.set_coefficient(this.get_coefficient()+other.get_coefficient());
+
+		if (this.get_coefficient()==0)
+			this.set_power(0);
+	}
+
+	/**
+	 * This method multiply this Monom by other Monom.
+	 * Multiply the coefficients and add the power
+	 * @param other the other Monom's coefficient and power
+	 * @throws Exception if the powers value is lower than zero
+	 */
+
+	public void multiply(Monom other) throws Exception {
+		this.set_coefficient(this.get_coefficient()*other.get_coefficient());
+		this.set_power(this.get_power()+other.get_power());
+		if (this.get_coefficient()==0)
+			this.set_power(0);
+	}
+
+	/**
+	 * This method derivative this Monom.
+	 * Change this Monom to the derivative of this Monom
+	 * Multiply the coefficient by power and subtract the power by one
+	 * @throws Exception if the powers value is lower than zero
+	 */
+
+	public void derivative() throws Exception {
+		// Checking if power bigger than 0 or equal to 0
+		// Power equal to 0. return 0.0 
+		if (this.get_power()==0) 
+			this.set_coefficient(0);
+		// Power bigger than 0. return a*x^b 
+		else {
+			double a=(this.get_power()*this.get_coefficient());
+			int b=(this.get_power()-1);
+			this.set_coefficient(a);
+			this.set_power(b);
+		}
+	} 
+
+	/**
+	 * This method subtract other Monom from this Monom.
+	 * Subtract only if the power of this Monom equals to other Monom
+	 * @param other the other Monom's coefficient and power
+	 * @throws Exception if the powers value is lower than zero
+	 * @throws Execption if the Monoms have different power
+	 */
+
+	public void subtract(Monom other) throws Exception{
+
+		// Subtract only the coefficients, the power stays the same
+		// Subtract on condition that both Monoms have the same power or one of them is equal to zero
+		if (this.get_coefficient()==0 || other.get_coefficient()==0) {
+			if (this.get_coefficient()==0) {
+			this.set_coefficient(other.get_coefficient()*-1);
+			this.set_power(other.get_power());
+			}
+		}
+
+		else if (this.get_power()!=other.get_power())
+			throw new Exception("The power of the monoms is diffrent");
+
+		else 
+			this.set_coefficient(this.get_coefficient()-other.get_coefficient());
+
+		if (this.get_coefficient()==0)
+			this.set_power(0);
+	}
+
+
+	/**
+	 * This method prints the coefficient and the power the Monom
+	 * @return the String of this Monom
+	 */
+
+	public String toString() {
+
+		// Checking if coefficient equal to 0. return: 0.0
+		if (this.get_coefficient()==0)
+			return "0.0";
+		// Checking if coefficient equal to 1 and power is not equal to 0
+		else if (this.get_coefficient()==1 && this.get_power()!=0) {
+			// Checking if power equal to 1. return : x .instead of : 1*x^1
+			if (this.get_power()==1)
+				return "x";
+			// Checking if power equal to any other number. return : x^b .instead of : 1*x^b
+			else
+				return "x^"+this.get_power();
+		}
+
+		// Checking if coefficient is negative number its added in brackets : (-a)*x^b
+		if (this.get_coefficient()<0)
+			// Checking if power equals to 0. return : (-a)
+			if (this.get_power()==0)
+				return "("+this.get_coefficient()+")";
+		// Checking if power equals to 1. return : (-a)*x
+			else if (this.get_power()==1)
+				return "("+this.get_coefficient()+")"+"*x";
+		// Checking if power equals to any other number. return : (-a)*x^b
+			else
+				return "("+this.get_coefficient()+")"+"*x^" +this.get_power();
+
+		// Checking if power equal to 0. return : a .instead of : a*x^0
+		if (this.get_power()==0)
+			return this.get_coefficient()+"";
+		// Checking if power equal to 1. return : a*x .instead of : a*x^1
+		else if (this.get_power()==1)
+			return this.get_coefficient()+"*x";
+
+		// Checking if both coefficient and power are bigger than 1 return : a*x^b
+		return this.get_coefficient()+"*x^" +this.get_power();
+
+	}
+	
+	public boolean isEquals(Monom other) {
+		if (this.get_coefficient()==other.get_coefficient() && this.get_power()==other.get_power())
+			return true;
+		return false;
+
+	}
+
+	//Getters
+
+	/**
+	 * This method get the coefficint of the Monom
+	 * @return coefficient
+	 */
+
+	public double get_coefficient() {
+		return _coefficient;
+	}
+
+	/**
+	 * This method get the power of the Monom
+	 * @return power
+	 */
+
+	public int get_power() {
+		return _power;
+	}
+
+	//****************** Private Methods and Data *****************
+
+	//Setters
+
+	/**
+	 * This method set the coefficient of the Monom
+	 * @param a the value of the coefficient
+	 */
+
+	private void set_coefficient(double a){
+		this._coefficient = a;
+	}
+
+	/**
+	 * This method set the power of the Monom.
+	 * Set the power if its bigger or equal to 0
+	 * @param b the value of the power
+	 * @throws Exception if the powers value is lower than zero
+	 */
+
+	private void set_power(int b) throws Exception  {
+		if(b>= 0) 
+			this._power = b;
+		else 
+			throw new Exception("The power should be greater than or equal to 0");
+	}
+
+
+	private double _coefficient; //Represent a : a*x^b
+	private int _power; //Represent b : a*x^b
+
+}
---
+++ src/myMath/Polynom.java
@@ -0,0 +1,529 @@
+package myMath;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import myMath.Monom;
+import myMath.Monom_Comperator;
+
+/**
+ * This class represents a Polynom with add, multiply functionality, it also should support the following:
+ * 1. Riemann's Integral: https://en.wikipedia.org/wiki/Riemann_integral
+ * 2. Finding a numerical value between two values (currently support root only f(x)=0).
+ * 3. Derivative
+ * 
+ * @author Chen Ostrovski 316402650, Ester Reznikov 315674028
+ */
+public class Polynom implements Polynom_able{
+	private ArrayList<Monom> Polynom_able;	
+
+	//Constructors
+
+	/** 
+	 * Zero Constructor : builds a new empty Polynom
+	 */	
+
+	public Polynom() {
+		Polynom_able= new ArrayList<>();
+	}
+
+	
+	/**
+	 *  Copy Constructor : deep copy, builds a new Polynom
+	 *  @param other get the Monoms from other Polynom and them in this Polynom
+	 *  @throws Exception if one of the Monoms in the Polynom have power the lower than zero
+	 */
+
+	public Polynom(Polynom other)  throws Exception{
+		Polynom_able= new ArrayList<>();
+		// The iterator help us to go through the Polynom
+		Iterator<Monom> p_iter = other.iteretor();
+		// While the other Polynom has Monoms, add the Monoms to this Polynom
+		while (p_iter.hasNext()) {
+			Monom current= new Monom(p_iter.next());
+			this.add(current);
+		}
+	}
+
+	/** 
+	 *  String Constructor : builds a new Polynom,
+	 *  Get String as input and change it to Polynom
+	 *  @param polynom proper input f(x) = a_1x^b_1 + a_2*x^b_2 ... a_n*xb_n
+	 *  @throws Exception if the Polynom equals to null
+	 *  @throws Exception if the Polynom contains invalid signs
+	 */
+
+	public Polynom(String polynom) throws Exception{
+		// throws exception if the Polynom equal to null
+		if (polynom==null)
+			throw new Exception("String is null");
+		else {
+			Polynom PolynomNew=new Polynom();
+			// Replaces the string marks so that the string matches the Monom constructor
+			polynom=polynom.replaceAll("[(,), ]","");
+			polynom=polynom.replaceAll("[-]"+"[-]", "+");
+			polynom=polynom.replaceAll("[-]", "+-");
+			polynom=polynom.replaceAll("[+]"+"[+]", "+");
+			if (polynom.charAt(0)=='+') {
+				polynom=polynom.replaceFirst("[+]", "");
+			}
+			// Split makes separation between Monoms
+			String[]s=polynom.split("\\+");
+			for (int i=0; i<s.length; i++) {
+				// Saves the Monoms values and adds to the this Polynom
+				Monom newM=new Monom(s[i]);
+				PolynomNew.add(newM);
+			}
+			this.Polynom_able=PolynomNew.Polynom_able;
+		}
+	}
+
+	//Methods
+
+	/**
+	 * This method add other Polynom to this Polynom.
+	 * Does not contain two Monoms with the same power,
+	 * If the Monom equal to 0 don't add it
+	 * @param p1 all the Monoms in other Polynom
+	 * @throws Exception if one of the Monoms in the Polynom have power the lower than zero
+	 */
+	@Override
+	public void add(Polynom_able p1) throws Exception {
+		if (p1!=null) {
+			// The iterator help us to go through the Polynom
+			Iterator<Monom> p_iter=p1.iteretor();
+			// While the other Polynom has Monoms add them to this Polynom
+			while (p_iter.hasNext() ) {
+				Monom current=p_iter.next();
+				this.add(current);
+			}
+		}
+	}
+
+	/**
+	 * This method add Monom to this Polynom.
+	 * Add two Monoms with the same power in order to avoid duplication,
+	 * If the Monom equal to 0 don't add it
+	 * @param m1 the Monom's coefficient and power
+	 * @throws Exception if the Monom have power that lower than zero
+	 */
+	@Override
+	public void add(Monom m1) throws Exception {
+		// Checking if the Monom is not equal to 0
+		if (m1.get_coefficient()!=0) {
+			boolean flag= true;
+			// The iterator help us to go through the Polynom
+			Iterator<Monom> p_iter=this.iteretor();
+			// Go over the Polynom and see if there is a Monom with the same power as m1
+			while (p_iter.hasNext() && flag ) {
+				Monom current=p_iter.next();
+				// Checking if the Monoms has the same power
+				if (m1.get_power()== current.get_power()) {
+					current.add(m1);
+					// If it found two Monons with the same power don't go through the while again
+					flag =false;
+				}
+			}
+			// If it didn't found two Monons with the same power add the Monom in the end of the Polynom
+			if (!p_iter.hasNext() && flag) {
+				Polynom_able.add(Polynom_able.size(), m1);
+			}
+			// If there are Monoms that equal to 0 remove them and sort the Polynom
+			Remove_Zero();
+		}
+	}
+
+	/**
+	 * This method subtract other Polynom from this Polynom
+	 * @param p1 all the Monoms in other Polynom
+	 * @throws Exception if one of the Monoms in the Polynom have power that lower than zero
+	 */
+	@Override
+	public void substract(Polynom_able p1) throws Exception {
+		// The iterator help us to go through the Polynom
+		Iterator<Monom> p_iter=p1.iteretor();
+		while (p_iter.hasNext() ) {
+			Monom current=p_iter.next();
+			// Subtract from this Polynom the Monoms from other Polynom
+			this.subtract(current);
+		}
+	}
+
+	/**
+	 * This method subtract Monom from this Polynom
+	 * @param m1 the Monom's coefficient and power
+	 * @throws Exception if the Monom have power that lower than zero
+	 */
+
+	public void subtract(Monom m1) throws Exception{
+		boolean flag= true;
+		// The iterator help us to go through the Polynom
+		Iterator<Monom> p_iter=this.iteretor();
+		// Go over the Polynom and see if there is a Monom with the same power as m1
+		while (p_iter.hasNext() && flag ) {
+			Monom current=p_iter.next();
+			// If the current Monoms in the Polynom has the same power as m1. subtract from current m1.
+			if (m1.get_power()== current.get_power()) {
+				current.subtract(m1);
+				// If it found two Monons with the same power don't go through the while again
+				flag =false;
+			}
+		}
+		// If it didn't found two Monons with the same power subtract the Monom from the end of the Polynom
+		if (!p_iter.hasNext() && flag) {
+			// Create a new Monom with coefficient equals to 0 and the same power as m1, subtract from it m1.
+			Monom m2=new Monom(0,m1.get_power());
+			m2.subtract(m1);
+			// Add to the Polynom m2. m2 equals to the negative m1
+			Polynom_able.add(Polynom_able.size(), m2);
+		}
+		// If there are Monoms that equal to 0 remove them and sort the Polynom
+		Remove_Zero();
+	}
+
+	/**
+	 * This method multiply this Polynom by other Polynom,
+	 * Multiply all the Monoms is this Polynom by the Monoms in the other Polynom
+	 * @param p1 all the Monoms in other Polynom
+	 * @throws Exception if one of the Monoms in the Polynom have power that lower than zero
+	 */
+
+	@Override
+	public void multiply(Polynom_able p1) throws Exception {
+		// Create two iterators one to go through this Polynom and one to go through other Polynom
+		Iterator<Monom> p_iter; 
+		Iterator<Monom> this_iter=this.iteretor(); // The iterator of this Polynom
+
+		Polynom copy_polynom=  new Polynom(); // Place to put the values after multiplying them
+		ArrayList<Monom> copy_polynom2=  new ArrayList<Monom>();
+		Monom temp=new Monom();
+		// Passes all the Monoms in this Polynom
+		while (this_iter.hasNext()) {
+			// thiscurrent represent the current Monom from this Polynom
+			Monom thiscurrent=this_iter.next();
+			p_iter=p1.iteretor(); // The iterator of other Polynom
+			// Passes all the Monoms in other Polynom
+			while (p_iter.hasNext()) {
+				// Put the Monom from this Polynom in temporary variable
+				temp=new Monom(thiscurrent);
+				// p1current represent the current Monom from other Polynom
+				Monom p1current=p_iter.next();
+				// Multiply the Monom from this Polynom by the Monom from other Polynom
+				temp.multiply(p1current);
+				// Add to the new Polynom
+				copy_polynom.add(temp);
+			}
+		}
+		// The iterator help us to go through the copy_polynom
+		Iterator<Monom> copy= copy_polynom.iteretor();
+		// transfers the Monoms form the Polynom to the ArrayList
+		while(copy.hasNext())
+			copy_polynom2.add(copy.next());
+		Polynom_able=copy_polynom2;
+		// If there are Monoms that equal to 0 remove them and sort the Polynom
+		Remove_Zero();
+
+	}
+
+	/**
+	 * This method check if this Polynom is logically equals to other Polynom
+	 * @param p1 all the Monoms in other Polynom
+	 * @return true : if this Polynom represents the same function or false : otherwise
+	 * @throws Exception if one of the Monoms in the Polynom have power that lower than zero
+	 */
+
+	@Override
+	public boolean equals (Polynom_able p1) throws Exception{
+		// Create two iterators one to go through this Polynom and one to go through other Polynom
+		Iterator<Monom> p_iter=p1.iteretor();
+		Iterator<Monom> this_iter=this.iteretor();
+		// While both Polynoms still have Monoms
+		while (this_iter.hasNext() && p_iter.hasNext()) {
+			Monom thiscurrent= new Monom (this_iter.next());
+			Monom p1current= new Monom (p_iter.next());
+			// Compare between the Monoms
+			// If they don't have the same coefficient or power return : False
+			if (thiscurrent.get_coefficient()!=p1current.get_coefficient()) 
+				return false;
+
+			if(thiscurrent.get_power()!=p1current.get_power()) 
+				return false;
+
+		}
+		// If both have no more Monoms return : True
+		if (!this_iter.hasNext() && !p_iter.hasNext())
+			return true;
+		return false;
+	}
+
+	/**
+	 * This method check if it's the Zero Polynom 
+	 * @return true : if the Polynom is null or false : otherwise
+	 */
+	@Override
+	public boolean isZero() {
+		return (Polynom_able.isEmpty());
+	}
+
+	/**
+	 * Compute a value of x (x bigger or equal to x0 and x smaller or equal to x1,
+	 * for f(x) smaller than eps),
+	 * assuming that(f(x0)*f(x1)) smaller or equal to 0, returns f(x2) such that:
+	 * (i) x0 bigger or equal to x1 bigger or equal to x2 and (ii) f(x2) smaller than eps
+	 * @param x0 starting point of the range
+	 * @param x1 ending point point of the range
+	 * @param eps step (positive) value
+	 * @throws Exception if epsilon is negative number
+	 * @throws Exception if the range is invalid
+	 * @return mid
+	 */
+	@Override
+	public double root(double x0, double x1, double eps) throws Exception {
+		double mid=0;
+		// epsilon have to be bigger than zero
+		if (eps<=0)
+			throw new Exception("eps need to be bigger than zero");
+		// x0 can't be bigger than x1
+		else if (x0>x1)
+			throw new Exception("x1 should be bigger than x0");
+		// All the values are legal
+		else { 
+			while ( (x1-x0) > eps )
+			{
+				mid=((x1+x0)/2);          
+				// Calculate the value of the Polynom at mid
+				double f_mid=this.f(mid);
+				// Calculate the value of the Polynom at x0
+				double f_x0=this.f(x0);
+				// f(x0) and f(mid) have different signs
+				if ( (f_mid > 0 && f_x0 < 0) || (f_mid < 0 && f_x0 > 0) ){
+					x1 = mid;
+				}
+				// f(x0) and f(mid) have same signs: move a
+				else{  
+					x0 = mid;
+				}
+			}
+		}
+		return mid;
+	}
+
+	/**
+	 * This method create a deep copy of this Polynum
+	 * @return newp deep copy of this Polynum
+	 * @throws Exception if one of the Monoms have power the lower than zero
+	 */
+	@Override
+	public Polynom_able copy()  throws Exception {
+		Polynom_able newp= new Polynom() ;
+		newp.add(this);
+		return newp;
+	}
+
+	/**
+	 * This method derivative this Polynom,
+	 * Change this Polynom to the derivative of this Polynom
+	 * @return newp derivative version of this Polynom
+	 * @throws Exception if one of the Monoms have power the lower than zero
+	 */
+	@Override
+	public Polynom_able derivative() throws Exception{
+		// Create a new Polynom in order to contain the derivative Monoms
+		Polynom_able newp= new Polynom();
+		// The iterator help us to go through the Polynom
+		Iterator<Monom> p_iter=this.iteretor();
+		// Passes all the Monoms in this Polynom
+		while (p_iter.hasNext() ) {
+			Monom current=p_iter.next();
+			// Checking if the Monom is not a free number
+			if (current.get_power()!=0) {
+				Monom derivativeMonom=new Monom(current);
+				derivativeMonom.derivative();
+				// Add to the new Polynom the derivative Monoms
+				newp.add(derivativeMonom);
+			}
+		}
+		return newp;
+	}
+	/**
+	 * Compute Riemann's Integral over this Polynom starting from x0, till x1 using eps size steps,
+	 * see: https://en.wikipedia.org/wiki/Riemann_integral
+	 * @param x0 starting point of the range
+	 * @param x1 ending point point of the range
+	 * @return the approximated area above the x-axis below this Polynom and between the [x0,x1] range.
+	 * @throws Exception if epsilon is negative number
+	 * @throws Exception if the range is invalid
+	 * @throws Exception if there is no cutting point with the X-axis
+	 */
+	@Override
+	public double area(double x0,double x1, double eps) throws Exception {
+		// Count the area above and under the X axis and the Polynom
+		return underArea(x0, x1,eps)+aboveArea(x0,x1,eps);
+	}
+
+	/**
+	 * Auxiliary function the calculates the area if its under the X-axis
+	 * @param x0 starting point of the range
+	 * @param x1 ending point point of the range
+	 * @return the approximated area under the x-axis above this Polynom and between the [x0,x1] range.
+	 * @throws Exception if there is no cutting point with the X-axis
+	 */
+	public double underArea(double x0,double x1, double eps) throws Exception {
+		// Throw exception if the Polynom don't cuts the X-axis
+		if (!xaxis(x0,x1,eps))
+			throw new Exception("There is no cutting point with the X-axis");
+		// Epsilon can't be a negative number or equal to 0 
+		else if (eps<=0)
+			throw new Exception("eps need to be bigger than zero");
+		// x0 can't be bigger than x1
+		else if (x0>x1)
+			throw new Exception("x1 should be bigger than x0");
+		else {
+
+			int n=(int)((x1-x0)/eps);
+			double sum=0;
+			for (int i=1; i<=n; i++) {
+				double xi=x0+eps*(i-1);
+				// Calculate the value of x in the i place
+				double f=this.f(xi); 
+				if (f<0)
+				sum=sum+f;
+			}
+			return Math.abs(sum*eps);
+		}
+	}
+	/**
+	 * Auxiliary function the calculates the area if its above the X-axis
+	 * @param x0 starting point of the range
+	 * @param x1 ending point point of the range
+	 * @param eps the size of steps
+	 * @return the approximated area above the x-axis under this Polynom and between the [x0,x1] range.
+	 * @throws Exception if there is no cutting point with the X-axis
+	 */
+	public double aboveArea(double x0,double x1, double eps) throws Exception {
+		
+		// Throw exception if the Polynom don't cuts the X-axis
+		if (!xaxis(x0,x1,eps))
+			throw new Exception("There is no cutting point with the X-axis");
+		// Epsilon can't be a negative number or equal to 0 
+		else if (eps<=0)
+			throw new Exception("eps need to be bigger than zero");
+		// x0 can't be bigger than x1
+		else if (x0>x1)
+			throw new Exception("x1 should be bigger than x0");
+		else {
+			int n=(int)((x1-x0)/eps);
+			double sum=0;
+			for (int i=1; i<=n; i++) {
+				double xi=x0+eps*(i-1);
+				// Calculate the value of x in the i place
+				double f=this.f(xi); 
+				if (f>0)
+				sum=sum+f;
+			}
+			return sum*eps;
+		}
+	}
+	/**
+	 * This method check if there is no cutting point with the X-axis
+	 * @param x0 starting point of the range
+	 * @param x1 ending point point of the range
+	 * @param eps the size of steps
+	 * @return true: Polynom cut the X-axis false: Polynom don't cut the X-axis
+	 * @throws Exception
+	 */
+	private boolean xaxis(double x0,double x1,double eps) throws Exception {
+		boolean flag=false;
+		// Checks if the Polynom cuts the X-axis
+		for (double i=x0; i<=x1; i+=0.001) {
+			if (Math.floor(this.f(i)*100)/100==0) {
+				flag=true;
+			}
+		}
+		return flag;
+	}
+	/**
+	 * This method help us to go through the Polynom
+	 * @return Iterator
+	 */
+	@Override
+	public Iterator<Monom> iteretor(){
+		Iterator<Monom> iter= Polynom_able.iterator();
+		return iter;
+	}
+	/**
+	 * This method prints the Polynom,
+	 * Prints " + " between the Monoms
+	 */
+
+	public String toString() {		
+		if (this.isZero())
+			return "0";
+		else {
+			String polynom=new String("");
+			// The iterator help us to go through the Polynom
+			Iterator<Monom> print_iter = this.iteretor();
+			while (print_iter.hasNext()) {
+				// Prints the Monoms
+				polynom=polynom+(print_iter.next()).toString();
+				// Prints " + " between the Monoms
+				if (print_iter.hasNext()) {
+					polynom=polynom+" + ";
+				}
+			}
+			return polynom;
+		}
+	}
+	/**
+	 * This method is used for build a new plot.
+	 * Given a Polynom and range the method draws a new plot
+	 * @param x1 the starting point of the range
+	 * @param x2 the ending point of the range
+	 * @throws Exception if the user build a invalid Polynom
+	 * @throws Exception if x1 is bigger than x2 (invalid range) 
+	 */
+	public void toPlot(double x1, double x2) throws Exception{
+		Polynom toplot=new Polynom(this.toString());
+		Plot plot=new Plot(toplot,x1,x2);
+		plot.setVisible(true);
+	}
+	/** 
+	 * This method get a real number and calculates the Polynom value
+	 * @param x the value of the number
+	 * @return the value of the Polynom at x
+	 * @throws Exception if the powers value is lower than zero
+	 * @throws Exception if the Monoms equals to null
+	 */
+
+	@Override
+	public double f(double x) throws Exception{
+		// The iterator help us to go through the Polynom
+		Iterator<Monom> iter = this.iteretor();
+		// Count the value of all the Monoms together
+		double sum=0;
+		while(iter.hasNext()) {
+			Monom current=new Monom(iter.next());
+			// Add the Monom value to the sum
+			sum=sum+current.f(x);
+		}
+		return sum;
+	}
+	/**
+	 * This method remove zero Monom from the Polynom,
+	 * remove from the Polynom Monoms that their coefficient is equal to zero
+	 */
+	private void Remove_Zero() {
+		// The for loop help us to go through the Polynom
+		for (int i=0; i<this.Polynom_able.size(); i++) {
+			if (this.Polynom_able.get(i).get_coefficient()==0)
+				Polynom_able.remove(this.Polynom_able.get(i));
+		}
+		// Sort the Polynom by power (from the biggest to the smallest)
+		Collections.sort(this.Polynom_able,new Monom_Comperator()); 
+	}
+
+}
